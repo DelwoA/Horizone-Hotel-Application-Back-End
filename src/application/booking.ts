53a4f5e05b8b0865e4877de9b509e1d4c1bdc400
Delwo Athauda <delwo2003@gmail.com>
2025-03-21 04:52:19 +0530

    Populate user details in getAllBookingsForHotel endpoint by fetching user data from Clerk and attaching (id, firstName, lastName) to each booking.

--- src/application/booking.ts
+++ src/application/booking.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import Booking from "../infrastructure/schemas/Booking";
 import { createBookingDTO } from "../domain/dtos/booking";
+import { clerkClient } from "@clerk/express";
 
 export const createBooking = async (
   req: Request,
@@ -11,7 +12,7 @@ export const createBooking = async (
     // Zod validator 'createBookingDTO' used.
     const booking = createBookingDTO.safeParse(req.body);
 
-    // Checking if the updated hotel is in the shape of 'updateHotelDTO'
+    // Checking if the updated hotel request body is in the shape of 'createBookingDTO'
     if (!booking.success) {
       throw new Error(booking.error.message);
     }
@@ -57,10 +58,27 @@ export const getAllBookingsForHotel = async (
 ) => {
   try {
     const hotelId = req.params.hotelId;
-    const bookings = await Booking.find({ hotelId: hotelId }).populate(
-      "userId"
+    const bookings = await Booking.find({ hotelId: hotelId });
+    const bookingsWithUser = await Promise.all(
+      bookings.map(async (el) => {
+        const user = await clerkClient.users.getUser(el.userId);
+        console.log(user);
+        return {
+          _id: el._id,
+          hotelId: el.hotelId,
+          checkIn: el.checkIn,
+          checkOut: el.checkOut,
+          roomNumber: el.roomNumber,
+          user: {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+          },
+        };
+      })
     );
-    res.status(200).json(bookings);
+
+    res.status(200).json(bookingsWithUser);
     return;
   } catch (error) {
     next(error);
